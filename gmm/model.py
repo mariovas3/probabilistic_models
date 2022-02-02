a198942c5e47aec897bdf2c359bcdf9416f140d3
Mario Vasilev <mariovasilev212@gmail.com>
2022-02-02 07:39:33 +0000

    added functions for updating parameters in the M step of the EM algorithm and I wrote tests for them in test_model.py;

--- gmm/model.py
+++ gmm/model.py
@@ -11,6 +11,7 @@ class GMM:
         self.prior_weights = None
         self.determinants = None
         self.inverses = None
+        self.R_res = None
 
     def _init_params(self, dimensions: int):
         # uninformative prior;
@@ -22,7 +23,7 @@ class GMM:
         v = np.random.standard_normal(dimensions).reshape((-1, 1))
         H = np.eye(dimensions) - (2 / (v * v).sum()) * (v @ v.T)  # note the outer product on v;
         # generate the covariance matrices;
-        self.covariances = np.array([eigen_vals[i, :] * H @ H.T for i in range(self.num_mixtures)])
+        self.covariances = [eigen_vals[j, :] * H @ H.T for j in range(self.num_mixtures)]
         # precomute determinants for gaussian pdfs later to have O(1) time operations with determinants;
         self.determinants = [np.linalg.det(sigma) for sigma in self.covariances]
         # compute inverses once so that when computing gaussian pdf
@@ -30,25 +31,56 @@ class GMM:
         # observation x_i;
         self.inverses = [np.linalg.inv(sigma) for sigma in self.covariances]
 
-    def _compute_gaussian(self, x: np.ndarray, idx: int) -> float:
+    def _compute_gaussian(self, x: np.ndarray, idx: int) -> np.float64:
         d = self.means.shape[1]
         u = (x - self.means[idx, :])
         assert u.shape == (d,)
-        assert type(u @ self.inverses[idx] @ u.T) == np.float64
         # idx is the index of the mixture;
         return self.prior_weights[idx] * ((2 * np.pi) ** (-d/2)) * (self.determinants[idx] ** (-0.5)) * \
-                np.exp(-0.5 * u @ self.inverses[idx] @ u)  # precomputed inverse so matrix vector multiply takes O(d^2) time;
+                np.exp(-0.5 * u.T @ self.inverses[idx] @ u)  # precomputed inverse so matrix vector multiply takes O(d^2) time;
 
-    def _compute_responsibilities(self, X: np.ndarray) -> np.ndarray:
+    def _compute_many_gaussians(self, X: np.ndarray) -> np.ndarray:
         # X should be (N x d)
-        # responsibilities matrix should be (num_mixtures x N)
+        # the matrix of gaussians should be (num_mixtures x N)
+        # so that it can be used to compute the log-likelihood
+        # easily; and I can obtain the responsibilities by
+        # dividing by R.sum(0);
+        # Note: R[j, i] = p_j * N(x_i | mu_j, sigma_j)
         N, d = X.shape
         R = np.empty((self.num_mixtures, N))
-        for i in range(self.num_mixtures):
-             # compute gaussians on each row/observation;
-             R[i, :] = np.apply_along_axis(self._compute_gaussian, axis=1, idx=i, arr=X)
-        R = R / R.sum(0)
+        for j in range(self.num_mixtures):
+             # compute Gaussian pdfs multiplied by prior weights
+             # on each row/observation;
+             R[j, :] = np.apply_along_axis(self._compute_gaussian, axis=1, idx=j, arr=X)
+        self.R_res = R / R.sum(0)
         return R
+
+    def _compute_log_likelihood(self, R):
+        # recall R[j, i] = p_j * N(x_i | mu_j, sigma_j);
+        return np.log(R.sum(0)).sum()
+
+    def _update_prior_weights(self):
+        assert self.R_res is not None
+        self.prior_weights = self.R_res.sum(1) / self.R_res.shape[1]
+
+    def _update_covariances(self, X):
+        assert self.R_res is not None
+        for j in range(self.num_mixtures):
+            rj = self.R_res[j, :].reshape((-1, 1))
+            X_tilde = rj * (X - self.means[j, :])
+            # here this is a neat trick since in the Deisenroth book
+            # the update of an individual covariance matrix is given as a
+            # weighted sum of outer products sum_i(rji * (xi - mu_j) @ (xi - mu_j)^T)
+            # imagine rji * (xi - mu_j) is the i^th column of X_tilde 
+            # and (xi-mu_j)^T is just the i^th row of X - mu_j
+            # then the sum of outerproducts is just X_tilde @ (X - mu_j)
+            self.covariances[j] = (X_tilde.T @ (X - self.means[j, :])) / rj.sum()
+
+    def _update_means(self, X):
+        assert self.R_res is not None
+        # since mu_j^T = (X.T @ rj) ^ T / rj.sum()
+        # here R_res is (num_mixtures, N);
+        self.means = (self.R_res @ X) / self.R_res.sum(1).reshape((-1, 1))
 
     def fit(self, X: np.ndarray):
         raise NotImplementedError
--- gmm/test_model.py
+++ gmm/test_model.py
@@ -2,21 +2,70 @@ import numpy as np
 from model import GMM
 
 num_mixtures = 2
+N, d = 5, 3
 gmm = GMM(num_mixtures, 42)
-X = np.arange(1, 16).reshape(5, 3)
-gmm._init_params(X.shape[1])
-R = gmm._compute_responsibilities(X)
+X = np.random.normal(2, 4, size=(N, d))
+gmm._init_params(d)
+R = gmm._compute_many_gaussians(X)
 
 
 def test_init_params():
     assert gmm.prior_weights.sum().round(5) == 1
-    assert gmm.means.shape == (num_mixtures, X.shape[1])
+    assert gmm.means.shape == (num_mixtures, d)
     eigvals = np.concatenate([np.linalg.eigvals(sigma) for sigma in gmm.covariances])
     assert all(eigvals > 0)
     assert all(np.array(gmm.determinants) > 0)
 
 
-def test_compute_responsibilities():
-    assert all(R.sum(0).round(5) == 1)
-    assert R.sum() == X.shape[0]
-    assert R.shape == (num_mixtures, X.shape[0])
+def test_compute_many_gaussians():
+    assert (R > 0).sum() == R.size
+    assert all(gmm.R_res.sum(0).round(5) == 1)
+    assert gmm.R_res.sum() == N
+    assert gmm.R_res.shape == (num_mixtures, N)
+
+
+def test_compute_log_likelihood():
+    l = 0
+    for i in range(N):
+        temp = 0
+        for j in range(num_mixtures):
+            temp += R[j, i]
+        l += np.log(temp)
+    my_l = gmm._compute_log_likelihood(R)
+    assert l.round(5) == my_l.round(5)
+
+
+def test_update_prior_weights():
+    p = np.empty(num_mixtures)
+    for j in range(num_mixtures):
+        p[j] = gmm.R_res[j, :].sum() / N
+    gmm._update_prior_weights()
+    assert (p.round(5) == gmm.prior_weights.round(5)).all()
+
+
+def test_update_covariances():
+    covs = [np.zeros((d, d)) for j in range(num_mixtures)]
+    for j in range(num_mixtures):
+        rj_sum = gmm.R_res[j, :].sum()
+        for i in range(N):
+            u = (X[i, :] - gmm.means[j, :]).reshape((-1, 1))
+            covs[j] += gmm.R_res[j, i] * u @ u.T / rj_sum
+    # update my covariances stored in the gmm object;
+    gmm._update_covariances(X)
+    result = [(covs[i].round(5) == gmm.covariances[i].round(5)).sum()
+                for i in range(num_mixtures)]
+    assert np.sum(result) == num_mixtures * d * d
+
+
+def test_update_means():
+    mus = np.zeros((num_mixtures, d))
+    for j in range(num_mixtures):
+        rj_sum = gmm.R_res[j, :].sum()
+        for i in range(N):
+            mus[j] += gmm.R_res[j, i] * X[i, :] / rj_sum
+    # update my means stored in the gmm object;
+    gmm._update_means(X)
+    result = [(mus[j, :].round(5) == gmm.means[j, :].round(5)).sum()
+                for j in range(num_mixtures)]
+    assert np.sum(result) == num_mixtures * d
+
